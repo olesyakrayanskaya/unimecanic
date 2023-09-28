8b63af88131ced23072e705d583ac77bf2c22a98
Olesya <krayanskaya_ov@mail.ru>
2023-09-28 22:51:59 +0300

    index.html

--- practice/practice-1/src/js/modal.js
+++ practice/practice-1/src/js/modal.js
@@ -4,14 +4,17 @@ const openModal = document.querySelector('.open-form');
 
 openModal.addEventListener('click', () => {
   modal.style.display = 'block';
+  openModal.style.display = 'none';
 });
 
 closeModal.addEventListener('click', () => {
   modal.style.display = 'none';
+  openModal.style.display = 'block';
 });
 
 window.addEventListener('click', (event) => {
   if (event.target == modal) {
     modal.style.display = 'none';
+    openModal.style.display = 'block';
   }
 });
